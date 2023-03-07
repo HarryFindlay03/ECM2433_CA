36357109eac0a7fe9f7af656b4b730cf2b44ece5
Harry Findlay <harryfindlay@outlook.com>
2023-03-07 19:53:20 +0000

    using char pointers

--- q2/riffle.c
+++ q2/riffle.c
@@ -6,9 +6,9 @@ int main()
     int* work = (int *)malloc(sizeof(int) * (sizeof(L) / sizeof(L[0])));
     riffle_once(&L[0], 13, sizeof(int), work);
 
-    char* greek[12] = { "alpha", "beta", "gamma", "delta", "epsilon", "zeta", "eta", "theta", "iota", "kappa", "lambda", "mu" };    
-    char* work_greek[12] = {};
-    riffle_once(&greek[0], 12, sizeof(greek[0]), &work_greek[0]);
+    // char* greek[12] = { "alpha", "beta", "gamma", "delta", "epsilon", "zeta", "eta", "theta", "iota", "kappa", "lambda", "mu" };    
+    // char* work_greek[12] = {};
+    // riffle_once(&greek[0], 12, sizeof(greek[0]), &work_greek[0]);
 
     // output of shuffled numbers. 
     int i;
@@ -19,42 +19,66 @@ int main()
 void riffle_once(void* L, int len, int size, void* work)
 {
     // work pointer to set values in work
-    int* work_ptr = work;
+    char* work_ptr = work;
 
     // Setting pointers to the start and end of the list
-    int* start = L;
-    int* end = start + len - 1;
+    char* start = L;
+    char* end = start + ((len - 1) * size);
     
     // Find the centre point of the list
     int centre = len / 2;
     
     // Setting left and right pointers
-    int* left = start + centre - 1;
-    int* right = start + centre;
-
+    char* left = start + ((centre - 1) * size);
+    char* right = start + (centre * size);
+    
     // riffling
     srand(time(0));
     while(1)
     {
-        if(left == start - 1)
+        if(left == start)
+        {
+            *work_ptr = *left;
             break;
-        if(right == end + 1)
+        }
+        if(right == end)
+        {
+            *work_ptr = *right;
             break;
+        }
 
+        /* increase pointers by size */
         int choice = rand() % 2;
         if(choice)
-            *work_ptr++ = *left--;
-        else
-            *work_ptr++ = *right++;
+        {
+            *work_ptr = *left;
+            work_ptr += size;
+            left -= size;
+        } else {
+            *work_ptr = *right;
+            work_ptr += size;
+            right += size;
+        }
     }
+
     
     // adding remaining cards to deck
-    if(right == end + 1)
+    if(right == end)
     {
-        while(left != start - 1)
-            *work_ptr++ = *left--;
+        while(left != start)
+        {
+            *work_ptr = *left;
+            work_ptr += size;
+            left -= size;
+        }
+        *work_ptr = *left;
     } else{
-        while(right != end + 1)
-            *work_ptr++ = *right++;
+        while(right != end)
+        {
+            *work_ptr = *right;
+            work_ptr += size;
+            right += size;
+        }
+        *work_ptr = *right;
     }
 }
